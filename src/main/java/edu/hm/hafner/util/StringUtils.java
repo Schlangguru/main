2a9c2c59405196fa5c481aa9e116d308b6f8fe11
Felix Duerrwanger <duerrwan@hm.edu>
2013-04-22 23:07:48 +0200

    Updated StringUtils and StringUtilsTest.
    Added methods strip(), isSecure() and isValidIsbn13() in StringUtils.
    Added testcases in StringUtilsTest.

--- src/main/java/edu/hm/hafner/util/StringUtils.java
+++ src/main/java/edu/hm/hafner/util/StringUtils.java
@@ -1,5 +1,8 @@
 package edu.hm.hafner.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Several useful utility methods that work on {@link String} instances.
  *
@@ -26,4 +29,148 @@ public final class StringUtils {
     private StringUtils() {
         // prevents instantiation
     }
+
+    /**
+     * Entfernt in einem String alle vorkommenden Zeichen aus einem anderen String.
+     * Beispiel: Wenn man "Hallo-ollaH" mit "Halo" kürzt, ergibt das "-".
+     *
+     * @param eingabe Zu kürzender String
+     * @param toBeRemoved String mit den Zeichen, die entfernt werden sollen
+     * @return Gekürzter String
+     *
+     * @author Felix Dürrwanger
+     */
+    public static String strip(final String eingabe, final String toBeRemoved) {
+
+        if (eingabe == null || toBeRemoved == null) {
+            throw new IllegalArgumentException("Null is not a valid parameter");
+        }
+
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        for (char character : eingabe.toCharArray()) {
+            if (!toBeRemoved.contains(String.valueOf(character))) {
+                stringBuilder.append(character);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Prüf ein Passwort auf seine Sicherheit. Ein sicheres Passwort muss mindestens
+     * 20 Zeichen lang sein, davon 10 unterschiedliche. Es müssen je eine Zahl, ein
+     * kleiner Buchstabe, ein großer Buchstabe sowie ein Sonderzeichen vorkommen. Als
+     * Sonderzeichen werden alle Zeichen akzeptiert, die nicht in eine der anderen
+     * Kategorien fallen.
+     *
+     * @param passwordEingabe Zu prüfendes Passwort
+     * @return Passwort ist sicher
+     *
+     * @author Felix Dürrwanger
+     */
+    public static boolean isSecure(final String passwordEingabe) {
+
+        if (passwordEingabe == null || passwordEingabe.length() < 20) {
+            return false;
+        }
+
+        // Set speichert keine Duplikate
+        final Set<Character> characterSet = new HashSet<Character>();
+
+        for (char character : passwordEingabe.toCharArray()) {
+            characterSet.add(character);
+        }
+
+        // Länge des Arrays = Anzahl der verscheidenen Zeichen
+        if (characterSet.size() < 10) {
+            return false;
+        }
+
+        boolean numberFound = false;
+        boolean uppercaseFound = false;
+        boolean lowercaseFound = false;
+        boolean specialFound = false;
+
+        for (char character : characterSet) {
+            switch (Character.getType(character)) {
+                case Character.DECIMAL_DIGIT_NUMBER:
+                    numberFound = true;
+                    break;
+                case Character.UPPERCASE_LETTER:
+                    uppercaseFound = true;
+                    break;
+                case Character.LOWERCASE_LETTER:
+                    lowercaseFound = true;
+                    break;
+                default:
+                    specialFound = true;
+                    break;
+            }
+        }
+
+        return numberFound && uppercaseFound && lowercaseFound && specialFound;
+    }
+
+    /**
+     * Überprüft, ob der String eine gültige ISBN-13 ist. Eine ISBN-13 ist gültig,
+     * wenn die letzte Ziffer mit der Prüfsumme der ersten 12 übereinstimmt. Die
+     * Prüfsumme wird mit folgender Formel berechnet:
+     *
+     * (10 - ((z1 + z3 + z5 + z7 + z9 + z11 + 3 * (z0 + z2 + z4 + z6 + z8 + z10 + z12)) mod10)) mod10
+     *
+     * @param isbnEingabe Zu überprüfender String
+     * @return String ist gültige ISBN13
+     *
+     * @author Felix Dürrwanger
+     */
+    public static boolean isValidIsbn13(final String isbnEingabe) {
+
+        if (isbnEingabe == null) {
+            return false;
+        }
+
+        final char[] characters = removeBlanksAndHyphens(isbnEingabe).toCharArray();
+
+        if (characters.length != 13) {
+            return false;
+        }
+
+        final int[] numbers = new int[13];
+
+        for (int i = 0; i < 13; i++) {
+            if (Character.getType(characters[i]) != Character.DECIMAL_DIGIT_NUMBER) {
+                return false;
+            }
+            numbers[i] = Character.getNumericValue(characters[i]);
+        }
+
+        // Berechnung der Prüfsumme
+        final int checksum = (10 - ((numbers[0] + numbers[2] + numbers[4] + numbers[6] + numbers[8] + numbers[10]
+                + 3 * (numbers[1] + numbers[3] + numbers[5] + numbers[7] + numbers[9] + numbers[11])) % 10)) % 10;
+
+        return checksum == numbers[12];
+    }
+
+    /**
+     * Entfernt aus einem String alle Leerzeichen und Bindestriche.
+     *
+     * @param string String, aus dem die Leerzeichen und Bidnestriche entfernt werden
+     * @return String ohne Leerzeichen und Bindestriche
+     *
+     * @author Felix Dürrwanger
+     */
+    private static String removeBlanksAndHyphens(final String string) {
+
+        final StringBuilder stringBuilder = new StringBuilder();
+
+        for (char character : string.toCharArray()) {
+            final int type = Character.getType(character);
+            if (type != Character.SPACE_SEPARATOR && type != Character.DASH_PUNCTUATION) {
+                stringBuilder.append(String.valueOf(character));
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 }
--- src/test/java/edu/hm/hafner/util/StringUtilsTest.java
+++ src/test/java/edu/hm/hafner/util/StringUtilsTest.java
@@ -28,5 +28,118 @@ public class StringUtilsTest {
         assertFalse("Der String sollte nicht als empty erkannt werden", StringUtils.isEmpty("\t"));
         assertFalse("Der String sollte nicht als empty erkannt werden", StringUtils.isEmpty("Hallo"));
     }
+
+    /**
+     * Testet, ob die Zeichen alle aus dem String entfernt wurden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testeEntfernenVonZeichen() {
+
+        assertEquals("Ein leerer String kann nicht gekürzt werden und bleibt leer", StringUtils.strip("", "ABCabc123"), "");
+        assertEquals("Ein String, aus dem keine Zeichen entfernt werden sollen, ändert sich nicht", StringUtils.strip("ABCabc123", ""), "ABCabc123");
+        assertEquals("Haben die beiden Strings keine gleichen Zeichen, ändert sich der zu kürzende String nicht", StringUtils.strip("ABCabc123", "DEFdef456"), "ABCabc123");
+        assertEquals("Werden alle Zeichen entfernt, sollte der String leer sein", StringUtils.strip("ABCabc123", "ABCabc123"), "");
+        assertEquals("Nur das erste Zeichen sollte entfernt werden", StringUtils.strip("ABCabc123", "XAZ"), "BCabc123");
+        assertEquals("Nur das letzte Zeichen sollte entfernt werden", StringUtils.strip("ABCabc123", "X3Z"), "ABCabc12");
+        assertEquals("Nur die drei Zeichen in der Mitte sollten entfernt werden", StringUtils.strip("ABCabc123", "4a5b6c7"), "ABC123");
+        assertEquals("Nur die Leerzeichen sollten entfernt werden", StringUtils.strip("ABC abc 123", " "), "ABCabc123");
+    }
+
+    /**
+     * Testet, ob die Eingabe von null als ersten Parameter beim kürzen von Strings eine Exception wirft.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testeErstenParameterAufExceptionBeiNull() {
+        StringUtils.strip(null, "123ABCabc");
+    }
+
+    /**
+     * Testet, ob die Eingabe von null als zweiten Parameter beim Kürzen von Strings eine Exception wirft.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testeZweitenParameterAufExceptionBeiNull() {
+        StringUtils.strip("123ABCabc", null);
+    }
+
+    /**
+     * Testet, ob die Passwörter richtig auf ihre Sicherheit überprüft werden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testePasswortSicherheit() {
+        assertFalse("Null sollte ein unsicheres Passwort sein", StringUtils.isSecure(null));
+        assertFalse("Der Leerstring ist zu kurz und sollte ein unsicheres Passwort sein", StringUtils.isSecure(""));
+        assertFalse("Ein Password der Länge 19 ist zu kurz und sollte nicht sicher sein", StringUtils.isSecure("0123456789abcABC#%&"));
+        assertFalse("Ein Password ohne Zahl sollte unsicher sein", StringUtils.isSecure("abcdefghijABCDEFGHIJ#%&"));
+        assertFalse("Ein Password ohne Kleibuchstabe sollte unsicher sein", StringUtils.isSecure("1234567890ABCDEFGHIJ#%&"));
+        assertFalse("Ein Password ohne Großbuchstabe sollte unsicher sein", StringUtils.isSecure("1234567890abcdefghij#%&"));
+        assertFalse("Ein Password ohne Sonderzeichen sollte unsicher sein", StringUtils.isSecure("abcdefghijABCDEFGHI123"));
+        assertFalse("Ein Password mit 9 verschiedenen Zeichen sollte unsicher sein", StringUtils.isSecure("12abAB#%&&&&&&&&&&&&"));
+        assertTrue("Ein Password, das den Anforderungen entspricht, sollte sicher sein", StringUtils.isSecure("123456abcdefABCDEF#%&"));
+    }
+
+    /**
+     * Testet, ob ISBN-13-Nummern auch mit Leerzeichen und Bidnestrichen richtig erkannt werden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testeIsbn13MitLeerzeichenUndBindestrichen() {
+        assertTrue("Die ISBN-13 sollte mit Leerzeichen am Anfang gültig sein", StringUtils.isValidIsbn13(" 9780321356680"));
+        assertTrue("Die ISBN-13 sollte mit Leerzeichen am Ende gültig sein", StringUtils.isValidIsbn13("9780321356680 "));
+        assertTrue("Die ISBN-13 sollte mit Bindestrich am Anfang gültig sein", StringUtils.isValidIsbn13("-9780321356680"));
+        assertTrue("Die ISBN-13 sollte mit Bindestrich am Ende gültig sein", StringUtils.isValidIsbn13("9780321356680-"));
+        assertTrue("Die ISBN-13 sollte mit Leerzeichen an beliebiger Stelle gültig sein", StringUtils.isValidIsbn13("9 7 8 0 3 2 1 3 5 6 6 8 0"));
+        assertTrue("Die ISBN-13 sollte mit Bindestrichen an beliebiger Stelle gültig sein", StringUtils.isValidIsbn13("9-7-8-0-3-2-1-3-5-6-6-8-0"));
+    }
+
+    /**
+     * Testet, ob ISBN-13-Nummern mit falschen Zeichen als ungültig erkannt werden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testeIsbn13MitFalschenZeichen() {
+        assertFalse("Die ISBN-13 darf keine Buchstaben enthalten", StringUtils.isValidIsbn13("978A321356680"));
+        assertFalse("Die ISBN-13 darf keine Sonderzeichen enthalten", StringUtils.isValidIsbn13("978%321356680"));
+    }
+
+    /**
+     * Testet, ob ISBN-13-Nummern mit falscher Länge als ungültig erkannt werden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testeIsbnDreizehnMitFalscherLaenge() {
+        assertFalse("Die ISBN-13 darf nicht leer sein", StringUtils.isValidIsbn13(""));
+        assertFalse("Die ISBN-13 darf nicht null sein", StringUtils.isValidIsbn13(null));
+        assertFalse("Die ISBN-13 darf nicht zwölfstellig sein", StringUtils.isValidIsbn13("978032135668"));
+        assertFalse("Die ISBN-13 darf nicht vierzehnstellig sein", StringUtils.isValidIsbn13("97803213566801"));
+    }
+
+    /**
+     * Testet, ob ISBN-13-Nummern nur mit der richtigen Prüfsumme als gültig erkannt werden.
+     *
+     * @author Felix Duerrwanger
+     */
+    @Test
+    public void testeIsbn13MitFalscherPruefsumme() {
+
+        assertTrue("Die letzte Stelle der ISBN-13 muss mit der Prüfsumme übereinstimmen", StringUtils.isValidIsbn13("9780321356680"));
+
+        String isbnTrunk = "978032135668";
+
+        // Alle möglichen Prüfziffern testen
+        for (int i = 9; i > 0; i--) {
+            assertFalse("Die letzte Stelle der ISBN-13 muss mit der Prüfsumme übereinstimmen", StringUtils.isValidIsbn13(isbnTrunk + i));
+        }
+    }
 }
 
